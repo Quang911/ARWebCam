2f16de38352875f2c2e1af1f81e0fba1bffc38c5
QNM911 <quangnmptit@gmail.com>
2020-06-22 22:24:52 +0700

    first commit

---
+++ Assets/WebCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCam : MonoBehaviour
+{
+    public RawImage rawImg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        rawImg.texture = webcamTexture;
+        webcamTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
